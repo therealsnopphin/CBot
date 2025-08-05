f9a8cede981f13e444f67168d7df7832d2258b65
TwoOrSetp <truonghoangmai97@gmail.com>
2025-08-05 11:37:47 +0700

    Better Performance And Usage Meomries

--- test/fmodengine.h
+++ test/fmodengine.h
@@ -1,21 +1,20 @@
 #pragma once
-
+#include "pch.h"
 
 enum class Clicks
 {
 	Soft,
 	Normal
 };
-namespace CBot
-{
-	namespace fmodengine
-	{
-		void init();
-		void createSound(std::string file);
-		void playSound(std::string file, float Pitch, float Volume);
 
-		inline FMOD::System* system = nullptr;
-		inline std::unordered_map<std::string, FMOD::Sound*> sounds;
-		inline FMOD::Sound* sound = nullptr;
-	}
+namespace CBot::fmodengine
+{
+	inline FMOD::System* system = nullptr;
+	inline FMOD::Sound* sound = nullptr;
+	inline std::unordered_map<std::string, FMOD::Sound*> sounds;
+
+	void init();
+	void createSound(const std::string& file);
+	void createSound(const std::string& file, FMOD_MODE flags);
+	void playSound(const std::string& file, float pitch, float volume);
 }
